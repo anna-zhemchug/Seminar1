e2eb7d10edf87e2cff6bc3f376365dd304ed2d2c
Anna Zhemchugova <mio007@mail.ru>
2022-08-14 19:41:45 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Ведите число b: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+else
+{
+    Console.WriteLine($"max = {numberB}");
+}

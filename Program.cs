5814c4cb34ffe08aef635e8a5ac21c86862879a4
Anna Zhemchugova <mio007@mail.ru>
2022-08-14 19:56:08 +0300

    Задача 4

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Write("Введите число a: ");
 int numberA = int.Parse(Console.ReadLine());
-Console.Write("Ведите число b: ");
+Console.Write("Введите число b: ");
 int numberB = int.Parse(Console.ReadLine());
-if(numberA > numberB)
-{
-    Console.WriteLine($"max = {numberA}");
-}
-else
-{
-    Console.WriteLine($"max = {numberB}");
-}
+Console.Write("Введите число с: ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.WriteLine($"Максимальное число = {max}");
